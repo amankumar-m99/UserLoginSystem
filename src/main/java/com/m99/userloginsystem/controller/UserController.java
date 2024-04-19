ad3ce4babb82fc6281b42508febe6028a27809e7
Aman Kumar <amankumar.m99@gmail.com>
2024-04-20 02:36:44 +0530

    resolved a bug due to wrong keyword

--- src/main/java/com/m99/userloginsystem/controller/UserController.java
+++ src/main/java/com/m99/userloginsystem/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m99.userloginsystem.entity.user.User;
@@ -41,7 +41,7 @@ public class UserController {
 	}
 
 	@GetMapping({"/user-info/{username}"})
-	public User getUserInfo(@RequestParam String username) {
+	public User getUserInfo(@PathVariable String username) {
 		return this.userService.getUserByUsername(username);
 	}
 }
