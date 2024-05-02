01967c026154ab0e956a10c9743a3a7ce3f5d038
Aman Kumar <amankumar.m99@gmail.com>
2024-05-03 02:54:03 +0530

    added mutipart support

--- src/main/java/com/m99/userloginsystem/controller/user/UserController.java
+++ src/main/java/com/m99/userloginsystem/controller/user/UserController.java
@@ -9,12 +9,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.m99.userloginsystem.entity.user.User;
 import com.m99.userloginsystem.service.user.UserService;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.Multipart;
 
 @RestController
 @CrossOrigin
@@ -39,6 +43,12 @@ public class UserController {
 	@GetMapping({"/current-user"})
 	public String getCurrentUser(Principal principal) {
 		return principal.getName();
+	}
+
+	@PostMapping({"/profile-pic"})
+	public String saveUserProfilePic(@RequestParam("file") MultipartFile file) {
+		System.out.println(file.getOriginalFilename());
+		return file.getOriginalFilename();
 	}
 
 	@GetMapping({"/user-info/{userIdStr}"})
