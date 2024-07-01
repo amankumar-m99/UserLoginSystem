7604f2432edc8a7110535b7ed2803794fc523926
Aman Kumar <amankumar.m99@gmail.com>
2024-07-02 01:22:37 +0530

    wip cleaning process

--- src/main/java/com/m99/userloginsystem/utils/ConsolePrinter.java
+++ src/main/java/com/m99/userloginsystem/utils/ConsolePrinter.java
@@ -4,19 +4,13 @@ public class ConsolePrinter {
 
 	private static final boolean doPrint = true;
 	public static void print(String s) {
-		if(!doPrint)
-			return;
-		System.out.print(s);
-	}
-
-	public static void println(String s) {
-		if(!doPrint)
-			return;
-		System.out.println(s);
+		if(doPrint) {
+			System.out.println(s);
+		}
 	}
 
 	public static void handleException(Exception e, String message) {
-		println(message);
+		print(message);
 		e.printStackTrace();
 	}
 }
